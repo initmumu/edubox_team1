6b57904d696bdf0e554a070db3912ce5747de09f
배승호 <moomu.like@gmail.com>
2024-12-13 23:28:12 +0900

    INIT: 프로젝트 초기화 java 21 gradle: JPA, spring web

---
+++ src/main/java/com/gamja/edubox_team1/EduboxTeam1Application.java
@@ -0,0 +1,13 @@
+package com.gamja.edubox_team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduboxTeam1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduboxTeam1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamja/edubox_team1/EduboxTeam1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamja.edubox_team1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduboxTeam1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
