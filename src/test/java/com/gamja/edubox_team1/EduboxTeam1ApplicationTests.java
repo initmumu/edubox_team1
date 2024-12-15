122387a2b5c510be9576af277475b4083961c9fe
배승호 <moomu.like@gmail.com>
2024-12-15 23:36:22 +0900

    CHORE: 테스트용 서비스 함수 제거

--- src/test/java/com/gamja/edubox_team1/EduboxTeam1ApplicationTests.java
+++ src/test/java/com/gamja/edubox_team1/EduboxTeam1ApplicationTests.java
@@ -1,19 +1,10 @@
 package com.gamja.edubox_team1;
 
-import com.gamja.edubox_team1.auth.service.UserSetupService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EduboxTeam1ApplicationTests {
-
-	@Autowired
-	private UserSetupService userSetupService;
-
-	@Test
-	void createTestUser() {
-		userSetupService.createUser("testuser", "password123", "USER");
-	}
 
 }
